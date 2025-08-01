5bb5989d4a79b62aaa298809bfe7384b8807cf6f
patties-eater <anjugautam7223@gmail.com>
2025-08-01 22:03:51 +0545

    vrecel set up

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // Important for GitHub Pages and sometimes Vercel
+  base: "./prajjwalpotfolio", // Important for GitHub Pages and sometimes Vercel
   plugins: [react(),
     tailwindcss(),
   ],
